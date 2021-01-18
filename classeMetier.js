ab4b59e9348dbb96f1b712a74882466c0e15b8bf
Pierre-nick <pierre-nicolas.martinez@orange.fr>
2021-01-18 11:09:33 +0100

    attribus privés

---
+++ classeMetier.js
@@ -0,0 +1,11 @@
+class Voiture{
+    #immatriculation;
+    #couleur;
+    #poids = 0;
+    #puissance = 0;
+    #capReservoir = 0.0;
+    #nivEssence = 0.0;
+    #nbrePlace = 0;
+    #assure = true;
+    #messTab;
+}
