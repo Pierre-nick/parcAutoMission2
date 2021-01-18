600cdd695e5fc37edebb020230c9183f47b94844
Pierre-nick <pierre-nicolas.martinez@orange.fr>
2021-01-18 11:19:14 +0100

    constructeur

--- classeMetier.js
+++ classeMetier.js
@@ -1,11 +1,22 @@
 class Voiture{
     #immatriculation;
     #couleur;
-    #poids = 0;
-    #puissance = 0;
-    #capReservoir = 0.0;
-    #nivEssence = 0.0;
-    #nbrePlace = 0;
-    #assure = true;
+    #poids;
+    #puissance ;
+    #capReservoir;
+    #nivEssence ;
+    #nbrePlace ;
+    #assure ;
     #messTab;
+    constructor(immatriculation, couleur, poids, puissance, capReservoir, nbrePlace){
+        this.#immatriculation = immatriculation;
+        this.#couleur = couleur;
+        this.#poids = poids;
+        this.#puissance = puissance;
+        this.#capReservoir = capReservoir;
+        this.#nbrePlace = nbrePlace;
+        this.#nivEssence = 5;
+        this.#assure = false;
+        this.#messTab = console.log("coucou");
+    }
 }
